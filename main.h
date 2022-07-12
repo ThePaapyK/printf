803bf96dd896e020970c4e7da88c3f791e9cbf6c
amartey-tricky <trickya23@outlook.com>
2022-07-12 15:46:52 +0000

    fix error

--- main.h
+++ main.h
@@ -23,9 +23,7 @@ typedef struct func_s
 /* _putchar function */
 int _putchar(char c);
 
-/* conversion functions */
-int convert_alpha_num(int n, int upper);
-int *convert_base(unsigned long n, unsigned int base, int upper);
+
 
 /* Function that produces output according to a format */
 int _printf(const char *format, ...);
@@ -38,6 +36,10 @@ int print_percent(va_list ap);
 /* print_int functions */
 int print_int_helper(int n);
 int print_int(va_list ap);
+
+/* conversion functions */
+int convert_alpha_num(int n, int upper);
+int *convert_base(unsigned long n, unsigned int base, int upper);
 
 /* print_number functions */
 int print_binary(va_list ap);
